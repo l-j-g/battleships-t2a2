c6a7b0a6ee2d277d8047ba1504a896fd1196091b
l-j-g <lachlang93@gmail.com>
2021-10-01 12:12:01 +1000

    two way communication now fully functional

--- classes.py
+++ classes.py
@@ -6,6 +6,7 @@ import sys
 from art import * 
 import traceback
 from _thread import *
+from ast import literal_eval
 
 
 class Battleships:
@@ -37,7 +38,7 @@ class Battleships:
 	def draw(self):
 
 		tprint('BATTLESHIPS')
-		print("     " + Format.underline +"Player 1:" + Format.end)
+		print("    " + Format.underline +"Your Fleet:" + Format.end)
 		Battleships.print_board(self,self.board)
 		print("     " + Format.underline +"Opponent:" + Format.end)
 		Battleships.print_board(self,self.opp_board)
@@ -55,33 +56,17 @@ class Battleships:
 					element = ' ~'
 					print(element,end="")
 				if element == 1:
-					element = ' x'
+					element = Format.green + ' x' + Format.reset
 					print(element, end="")
 				if element == 2:
 					element = Format.red + ' x' + Format.reset
+					print(element,end="")
+				if element == 3:
+					element = ' o'
+					print(element, end="")
 			print("")   
 		return
 
-	def place_ships(self,ship):
-		direction = 'horizontal' if random.randint(0, 1) == 0 else 'vertical'
-		locations = []
-		if direction == 'horizontal':
-
-			for r in range (self.row_size):
-				for c in range (self.col_size - ship + 1):
-					if 1 not in self.board[r][c:c+ship]:
-						locations.append({'row' : r, 'col': c})
-			start_point = locations[random.randint(0,len(locations)-1)]                    
-			self.board[start_point['row'],start_point['col']:start_point['col']+ship] = 1
-		if direction == 'vertical':
-			for c in range (self.col_size):
-				for r in range (self.row_size - ship + 1):
-					if 1 not in [self.board[i][c] for i in range(r,r+ship)]:
-						locations.append({'row' : r, 'col': c})
-			start_point = locations[random.randint(0,len(locations)-1)]                    
-			self.board[start_point['row']:start_point['row']+ship,start_point['col']] = 1
-								
-		return 
 
 
 	def get_input(self, direction):
@@ -89,7 +74,7 @@ class Battleships:
 			try:
 				guess = int(input(f"{direction} Guess: "))
 				if guess in range(1, self.row_size +1):
-					guess - 1
+					guess = guess - 1
 					return guess 
 				else:
 					print("Selection out of range")
@@ -97,14 +82,52 @@ class Battleships:
 				print("Enter a number. ")
 
 	def check(self,attack):
+		print(f"Your Opponent fired at Row: {attack[0]+1}, Column {attack[1]+1}...", end="")
 		if self.board[attack[0],attack[1]] == 1:
 			# hit
+			print("and HIT!")
 			self.board[attack[0],attack[1]] = 2
+			return 1
 
 		if self.board[attack[0],attack[1]] == 0:
-			# miss 
-			pass
-    			
+			print("and Missed!!")
+			self.board[attack[0],attack[1]] = 3
+			return 0
+				
+	def update_opponent_board(self,guess):
+		print(guess)
+		if guess[2]== 0:
+			print("Bummer, you missed")
+			self.opp_board[guess[0],guess[1]] = 2
+
+		if guess[2] == 1:
+			print("Gottem")
+			self.opp_board[guess[0],guess[1]] = 1
+	
+	def manual_placement(self):
+		for ship in self.ships.values():
+
+			direction = 'horizontal' if random.randint(0, 1) == 0 else 'vertical'
+			locations = []
+
+			if direction == 'horizontal':
+				for r in range (self.row_size):
+					for c in range (self.col_size - ship + 1):
+						if 1 not in self.board[r][c:c+ship]:
+							locations.append({'row' : r, 'col': c})
+				start_point = locations[random.randint(0,len(locations)-1)]                    
+				self.board[start_point['row'],start_point['col']:start_point['col']+ship] = 1
+
+			if direction == 'vertical':
+				for c in range (self.col_size):
+					for r in range (self.row_size - ship + 1):
+						if 1 not in [self.board[i][c] for i in range(r,r+ship)]:
+							locations.append({'row' : r, 'col': c})
+				start_point = locations[random.randint(0,len(locations)-1)]                    
+				self.board[start_point['row']:start_point['row']+ship,start_point['col']] = 1
+		return 
+
+
 
 
 class Connection:
@@ -119,11 +142,34 @@ class Connection:
 
 		with self.socket as server:
 			self.set_up(server)
+
+			while self.game.ships_placed == False:
+				self.place_ships(server)
+
 			while game.health > 0 or game.opp_health > 0:
 				self.game.draw()	
 				self.take_turn(server)
 
+	def place_ships(self,server):
+		print("How would you like to place your ships?: (M)anual or (A)utomatic")
+		placement = input()
+		if placement[0].lower() == 'a':
+			self.game.manual_placement()
+
+		if self.role == 'server':
+			print("Waiting for opponent to place their ships...")
+			response = self.recieve(server)
+			self.send(server,response)
 			
+
+		if self.role == 'client':
+			self.send(server,'recieved')
+			print("Waiting for opponent to place their ships...")
+			response = self.recieve(server)
+
+		if response == 'recieved':
+			self.game.ships_placed = True	
+     			
 	def set_up(self,server):
 		if self.role == 'server': 
 			try:
@@ -155,11 +201,12 @@ class Connection:
 
 				
 	def send(self,s,message):
+		print(f"Sending {message}")
 		try:
 			if self.role =='server':
-				self.connection.sendall(bytes(message))
+				self.connection.sendall(bytes(message,'utf-8'))
 			if self.role == 'client':
-				s.sendall(bytes(message))
+				s.sendall(bytes(message,'utf-8'))
 		except Exception as e:
 			print(e)
 			print(traceback.format_exc())
@@ -169,17 +216,18 @@ class Connection:
 		try:
 			if self.role == "server":
 				while True:
-					message = list(self.connection.recv(1024))
-					print(message)
+					message = self.connection.recv(1024)
 					if not message:
 						break
+					print(f"Got {message}")
+					return message.decode('utf-8')
 			if self.role == "client":
 				while True:
-					message = list(s.recv(1024))
-					print(message)
+					message = s.recv(1024)
 					if not message:
 						break
-			return message
+					print(f"Got {message}")
+					return message.decode('utf-8')
 		except Exception as e:
 			print(e)
 			print(traceback.format_exc())
@@ -191,28 +239,50 @@ class Connection:
 
 	def take_turn(self,server):
 		guess = []
-		print("Connection Established!... Lets Get Started")
-		print(f"You are Player {self.game.player}")
+		result= ""
+		attack = ""
+
+		if self.game.turn == 1:
+			print("Connection Established!... Lets Get Started")
+			print(f"You are Player {self.game.player}")
 
 		if self.game.turn % 2 == 1:
 			if self.game.player == 1: 
 				guess.append(self.game.get_input("Row"))
 				guess.append(self.game.get_input("Column"))
-				self.send(server,guess)
+				self.send(server,repr(guess))
+				result = self.recieve(server)
+
+				print(result)
+
+				result = literal_eval(result)
+				self.game.update_opponent_board(result)
+
 			if self.game.player == 2: 
 				print("Waiting for Opponent to take their turn...")
-				self.recieve(server)
+				attack = literal_eval(self.recieve(server))
+				print(attack)
+				result = self.game.check(attack)
+				attack.append(result)
+				print(attack)
+				self.send(server,repr(attack))
 
 		if self.game.turn % 2 == 0:
 			if self.game.player == 2: 
 				guess.append(self.game.get_input("Row"))
 				guess.append(self.game.get_input("Column"))
-				server.send(server,guess)
+				self.send(server,repr(guess))
+
+				response = literal_eval(self.recieve(server))
+				self.game.update_opponent_board(response)
+
 			if self.game.player == 1: 
 				print("Waiting for Opponent to take their turn...")
 
-				attack = self.recieve(server)
-				self.game.check(attack)
+				attack = literal_eval(self.recieve(server))
+				response = self.game.check(attack)
+				attack.append(response)
+				self.send(server,repr(attack))
 
 		self.game.turn += 1
 		self.game.draw()
@@ -222,3 +292,5 @@ class Format:
 	underline = '\033[4m'
 	red = '\u001b[31m'
 	reset = '\u001b[0m'
+	blink = '\33[5m'
+	green = '\u001b[32m'
--- main.py
+++ main.py
@@ -5,22 +5,12 @@ import pdb
 
 game = Battleships()
 
-while game.ships_placed == False:
-    print("How would you like to place your ships?: (M)anual or (A)utomatic")
-    placement = input()
-    if placement[0].lower() == 'a':
-        for ship in game.ships.values():
-            game.place_ships(ship)
-
-    game.ships_placed = True
-
 while game.connection_established == False:
     print("Client or Server?")
     c_o_s = input()
     if c_o_s[0].lower() == 'c':
         game.player = 2
         server = Connection("client",game)
-
 
     if c_o_s[0].lower() == 's':
         server = Connection('server',game)
